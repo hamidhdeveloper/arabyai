7776c69e76e96af1766ba5669187e6ff603799ab
Hamid Hussain <hamidhussain740@gmail.com>
2023-02-16 19:22:54 +1100

    first task at amaas insverment compeleted in 4 days 6 hours

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <Routes>
         <Route path="/" element={ <Home /> } />
         <Route path="login" element={ <Login /> } />
+        {/* <Route path="signup" element={ <SignUpModel /> } /> */}
     </Routes>    
     </div>
     </BrowserRouter>;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
+import "../pages/signup/SignUpModel.css";
 const Header = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
+
   return (
     <div className="header">
       <div className="header-left">
@@ -13,10 +26,81 @@ const Header = () => {
       </div>
       <div className="header-right">
         <Link to="login">Login</Link>
-        <Link to="login"><img src="../assets/images/Login.svg" alt="img" /></Link>
-        <Link to="signup"><button>Sign Up</button></Link>
+        <Link to="login">
+          <img src="../assets/images/Login.svg" alt="img" />
+        </Link>
+        <button onClick={toggleModal}>Sign Up</button>
         <span>ع</span>
       </div>
+      {/* after header right popup */}
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <div className="mymodal">
+              <div className="modal-left">
+                <h1>
+                  Join Now And Receive A Special Gift <br />
+                  From Us
+                </h1>
+                <img src="../assets/images/realSignImg.svg" alt="modelimg" />
+              </div>
+              <div className="modal-right">
+                <h1>Create Account.. </h1>
+                <p>
+                  Get Access to Social Media, Emails, Articles,
+                  <br /> Advertisements, Images, and Much More
+                </p>
+                <div className="loginform2">
+                  <input
+                    type="text"
+                    placeholder="Enter your email"
+                    name="email"
+                  />
+                  
+                  <button className="loginbutton">Continue</button>
+                  
+                    <a href="#regmobile" className="regmobile">
+                       Register using Mobile Number?
+                    </a>
+                  
+                  <div className="line2">
+                    <div className="left-line2">
+                      <hr />
+                    </div>
+                    <div className="center-line2">Or Continue With</div>
+                    <div className="right-line2">
+                      <hr />
+                    </div>
+                  </div>
+                  <div className="twobutton">
+                  <button className="regfbAccountbutton">
+                   
+                    <img src="../assets/images/facebook.svg" alt="fb" />
+                    Facebook
+                  </button>
+                  <button className="regfbAccountbutton">
+                    
+                    <img src="../assets/images/googleIcon.svg" alt="gb" />
+                    Google
+                  </button>
+                  </div>
+                  <button className="regappleAccountbutton">
+                    
+                    <img src="../assets/images/apple.svg" alt="app" />
+                    Apple
+                  </button>
+                 
+                </div>
+              </div>
+            </div>
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+      {/* popup */}
     </div>
   );
 };
